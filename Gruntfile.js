c5d8e4b5780098fb110aba2461a7ff2b950a05d9
Michał Lach <kontakt@michal-lach.pl>
2014-09-03 15:06:11 +0200

    Added Grunt support for Heroku

--- Gruntfile.js
+++ Gruntfile.js
@@ -148,10 +148,9 @@ module.exports = function(grunt) {
 
   grunt.registerTask('build', function() {
     grunt.task.run([
-      'bower:install',
+      'bower',
       'concat',
       'less',
-      'uglify',
     ]);
   });
 
